42772fe703a333a5be4f3ecc323943156ac75b74
pimsp <spelier.pim@gmail.com>
2017-10-06 21:39:41 +0200

    Added CP3 geometry

---
+++ code/geometry/CP/circles.cpp
@@ -0,0 +1,14 @@
+int insideCircle(point_i p, point_i c, int r) { // all integer version
+  int dx = p.x - c.x, dy = p.y - c.y;
+  int Euc = dx * dx + dy * dy, rSq = r * r;             // all integer
+  return Euc < rSq ? 0 : Euc == rSq ? 1 : 2; } //inside/border/outside
+
+bool circle2PtsRad(point p1, point p2, double r, point &c) {
+  double d2 = (p1.x - p2.x) * (p1.x - p2.x) + 
+              (p1.y - p2.y) * (p1.y - p2.y);
+  double det = r * r / d2 - 0.25;
+  if (det < 0.0) return false;
+  double h = sqrt(det);
+  c.x = (p1.x + p2.x) * 0.5 + (p1.y - p2.y) * h;
+  c.y = (p1.y + p2.y) * 0.5 + (p2.x - p1.x) * h;
+  return true; }         // to get the other center, reverse p1 and p2
---
+++ code/geometry/CP/triangles.cpp
@@ -0,0 +1,75 @@
+double perimeter(double ab, double bc, double ca) {
+  return ab + bc + ca; }
+
+double perimeter(point a, point b, point c) {
+  return dist(a, b) + dist(b, c) + dist(c, a); }
+
+double area(double ab, double bc, double ca) {
+  // Heron's formula, split sqrt(a * b) into sqrt(a) * sqrt(b); in implementation
+  double s = 0.5 * perimeter(ab, bc, ca);
+  return sqrt(s) * sqrt(s - ab) * sqrt(s - bc) * sqrt(s - ca); }
+
+double area(point a, point b, point c) {
+  return area(dist(a, b), dist(b, c), dist(c, a)); }
+
+double rInCircle(double ab, double bc, double ca) {
+  return area(ab, bc, ca) / (0.5 * perimeter(ab, bc, ca)); }
+
+double rInCircle(point a, point b, point c) {
+  return rInCircle(dist(a, b), dist(b, c), dist(c, a)); }
+
+// assumption: the required points/lines functions have been written
+// returns 1 if there is an inCircle center, returns 0 otherwise
+// if this function returns 1, ctr will be the inCircle center
+// and r is the same as rInCircle
+int inCircle(point p1, point p2, point p3, point &ctr, double &r) {
+  r = rInCircle(p1, p2, p3);
+  if (fabs(r) < EPS) return 0;                   // no inCircle center
+
+  line l1, l2;                    // compute these two angle bisectors
+  double ratio = dist(p1, p2) / dist(p1, p3);
+  point p = translate(p2, scale(toVec(p2, p3), ratio / (1 + ratio)));
+  pointsToLine(p1, p, l1);
+
+  ratio = dist(p2, p1) / dist(p2, p3);
+  p = translate(p1, scale(toVec(p1, p3), ratio / (1 + ratio)));
+  pointsToLine(p2, p, l2);
+
+  areIntersect(l1, l2, ctr);           // get their intersection point
+  return 1; }
+
+double rCircumCircle(double ab, double bc, double ca) {
+  return ab * bc * ca / (4.0 * area(ab, bc, ca)); }
+
+double rCircumCircle(point a, point b, point c) {
+  return rCircumCircle(dist(a, b), dist(b, c), dist(c, a)); }
+
+// assumption: the required points/lines functions have been written
+// returns 1 if there is a circumCenter center, returns 0 otherwise
+// if this function returns 1, ctr will be the circumCircle center
+// and r is the same as rCircumCircle
+int circumCircle(point p1, point p2, point p3, point &ctr, double &r){
+  double a = p2.x - p1.x, b = p2.y - p1.y;
+  double c = p3.x - p1.x, d = p3.y - p1.y;
+  double e = a * (p1.x + p2.x) + b * (p1.y + p2.y);
+  double f = c * (p1.x + p3.x) + d * (p1.y + p3.y);
+  double g = 2.0 * (a * (p3.y - p2.y) - b * (p3.x - p2.x));
+  if (fabs(g) < EPS) return 0;
+
+  ctr.x = (d*e - b*f) / g;
+  ctr.y = (a*f - c*e) / g;
+  r = dist(p1, ctr);  // r = distance from center to 1 of the 3 points
+  return 1; }
+
+// returns true if point d is inside the circumCircle defined by a,b,c
+int inCircumCircle(point a, point b, point c, point d) {
+  return (a.x - d.x) * (b.y - d.y) * ((c.x - d.x) * (c.x - d.x) + (c.y - d.y) * (c.y - d.y)) +
+         (a.y - d.y) * ((b.x - d.x) * (b.x - d.x) + (b.y - d.y) * (b.y - d.y)) * (c.x - d.x) +
+         ((a.x - d.x) * (a.x - d.x) + (a.y - d.y) * (a.y - d.y)) * (b.x - d.x) * (c.y - d.y) -
+         ((a.x - d.x) * (a.x - d.x) + (a.y - d.y) * (a.y - d.y)) * (b.y - d.y) * (c.x - d.x) -
+         (a.y - d.y) * (b.x - d.x) * ((c.x - d.x) * (c.x - d.x) + (c.y - d.y) * (c.y - d.y)) -
+         (a.x - d.x) * ((b.x - d.x) * (b.x - d.x) + (b.y - d.y) * (b.y - d.y)) * (c.y - d.y) > 0 ? 1 : 0;
+}
+
+bool canFormTriangle(double a, double b, double c) {
+  return (a + b > c) && (a + c > b) && (b + c > a); }
