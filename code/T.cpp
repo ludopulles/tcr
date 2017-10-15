a68b9f8a3f19a78b242f85a30dbe464a3f47f781
Ludo Pulles <ludo.pulles@gmail.com>
2017-10-15 23:34:14 +0200

    Shorten convex hull, and changed some minor things in the template.cpp

--- code/T.cpp
+++ code/T.cpp
@@ -1,15 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Order statistics tree (if supported by judge!):
+// BBST + order statistics (if supported by judge)
+// iterator find_by_order(int r) (zero based)
+// int order_of_key(TK v)
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-
-template<class TK, class TM>
-using order_tree = tree<TK, TM, less<TK>, rb_tree_tag, tree_order_statistics_node_update>;
-// iterator	find_by_order(int r) (zero based)
-// int		order_of_key(TK v)
+template<class TK, class TM> using order_tree = tree<TK, TM, less<TK>, rb_tree_tag, tree_order_statistics_node_update>;
 template<class TV> using order_set = order_tree<TV, null_type>;
 
 #define x first
@@ -21,15 +19,16 @@ template<class TV> using order_set = order_tree<TV, null_type>;
 #define rs resize
 
 typedef long long ll;
+typedef long double ld;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 template<class T> using min_queue = priority_queue<T, vector<T>, greater<T>>;
-template <class T> int size(const T &x) { return x.size(); } // copy the ampersand(&)!
+template<class T> int size(const T &x) { return x.size(); } // copy the ampersand(&)!
 
-const int INF = 2147483647; // (1 << 30) - 1 + (1 << 30)
-const ll LLINF = (1LL << 62) - 1 + (1LL << 62); // = 9.223.372.036.854.775.807
-const double PI = acos(-1.0);
+const int INF = 2147483647;
+const ll LLINF = ~(1LL<<63); // = 9.223.372.036.854.775.807
+const ld PI = acos(-1.0);
 
 #ifdef LOCAL
 #define DBG(x) cerr << __LINE__ << ": " << #x << " = " << (x) << endl
@@ -39,15 +38,17 @@ const bool LOCAL = false;
 #endif
 
 void Log() { if(LOCAL) cerr << "\n\n"; }
-template<class T, class... S>
-void Log(T t, S... s) { if(LOCAL) cerr << t << "\t", Log(s...); }
-
+template<class T, class... S> void Log(T t, S... s) { if(LOCAL) cerr << t << "\t", Log(s...); }
 // lambda-expression: [] (args) -> retType { body }
+
+void run() {
+
+}
+
 int main() {
 	ios_base::sync_with_stdio(false); // fast IO
 	cin.tie(NULL); // fast IO
-	cerr << boolalpha; // print true/false
-	(cout << fixed).precision(10); // adjust precision
-
+	(cout << fixed).precision(18); // adjust precision
+	run();
 	return 0;
 }
--- code/geometry/convex_hull.cpp
+++ code/geometry/convex_hull.cpp
@@ -1,21 +1,18 @@
-// points are given by: pts[ret[0]], pts[ret[1]], ... pts[ret[ret.size()-1]]
+// the convex hull consists of: { pts[ret[0]], pts[ret[1]], ... pts[ret.back()] }
 vi convexHull(const vector<pt> &pts) {
 	if (pts.empty()) return vi();
-	vi ret;
-	// find one outer point:
-	int fsti = 0, n = pts.size(); pt fstpt = pts[0];
-	for(int i = n; i--; ) if (pts[i] < fstpt) fstpt = pts[fsti = i];
-	ret.pb(fsti); pt refr = pts[fsti];
-	vi ord; // index into pts
-	for (int i = n; i--; ) if (pts[i] != refr) ord.pb(i);
-	sort(ord.begin(), ord.end(), [&pts, &refr] (int a, int b) -> bool {
-		NUM cross = (pts[a] - refr) ^ (pts[b] - refr);
-		return cross != 0 ? cross > 0 : lenSq(refr, pts[a]) < lenSq(refr, pts[b]);
+	vi ret, ord;
+	int n = pts.size(), st = min_element(all(pts)) - pts.begin();
+	rep(i, 0, n)
+		if (pts[i] != pts[st]) ord.pb(i);
+	sort(all(ord), [&pts,&st] (int a, int b) {
+		pt p = pts[a] - pts[st], q = pts[b] - pts[st];
+		return (p ^ q) != 0 ? (p ^ q) > 0 : lenSq(p) < lenSq(q);
 	});
+	ret.pb(st);
 	for (int i : ord) {
-		// NOTE: > INCLUDES points on the hull-line, >= EXCLUDES
-		while (ret.size() > 1 &&
-				((pts[ret[ret.size()-2]]-pts[ret.back()]) ^ (pts[i]-pts[ret.back()])) >= 0)
+		// use '>' to include ALL points on the hull-line
+		for (int s = ret.size() - 1; s > 0 && ((pts[ret[s-1]] - pts[ret[s]]) ^ (pts[i] - pts[ret[s]])) >= 0; s--)
 			ret.pop_back();
 		ret.pb(i);
 	}
