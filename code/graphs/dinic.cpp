f91fe7bdb87ecf62e3b26488664a52c8cc8cefff
Ludo Pulles <ludo.pulles@gmail.com>
2018-10-31 21:20:29 +0100

    Update Dinic to how it should work...

--- code/graphs/dinic.cpp
+++ code/graphs/dinic.cpp
@@ -1,34 +1,37 @@
-struct edge { ll t, r, c, f; };
-int S, T, h[MAXN]; vector<edge> g[MAXN];
+struct Edge { int t; ll c, f; };
+struct Dinic {
+	vi H, P; vvi E;
+	vector<Edge> G;
+	Dinic(int n) : H(n), P(n), E(n) {}
 
-void addEdge(int u, int v, ll c) {
-	g[u].pb({v, (ll)g[v].size(), c, 0});
-	g[v].pb({u, (ll)g[u].size()-1,0,0});
-}
-void dinicBfs() {
-	fill_n(h, MAXN, 0); h[S] = 1;
-	queue<int> q; q.push(S);
-	while (!q.empty()) {
-		int v = q.front(); q.pop();
-		for (edge e : g[v]) if (!h[e.t] && e.f < e.c)
-				h[e.t] = h[v]+1, q.push(e.t);
+	void addEdge(int u, int v, ll c) {
+		E[u].pb(G.size()); G.pb({v, c, 0LL});
+		E[v].pb(G.size()); G.pb({u, 0LL, 0LL});
 	}
-}
-
-ll dinicDFS(int v, ll mf) {
-	if (v == T) return mf;
-	ll f = 0; bool sat = true;
-	for (edge &e : g[v]) {
-		if (h[e.t] != h[v] + 1 || e.f >= e.c) continue;
-		ll df = dinicDFS(e.t, min(mf - f, e.c - e.f));
-		f += df, e.f += df, g[e.t][e.r].f -= df;
-		sat &= e.f == e.c; if (mf == f) break;
+	ll dfs(int t, int v, ll f) {
+		if (v == t || !f) return f;
+		for ( ; P[v] < (int) E[v].size(); P[v]++) {
+			int e = E[v][P[v]], w = G[e].t;
+			if (H[w] != H[v] + 1) continue;
+			ll df = dfs(t, w, min(f, G[e].c - G[e].f));
+			if (df > 0) {
+				G[e].f += df, G[e ^ 1].f -= df;
+				return df;
+			}
+		} return 0;
 	}
-	if (sat) h[v] = 0;
-	return f;
-}
-
-ll dinicMaxFlow(ll f = 0) {
-	while (dinicBfs(), h[T]) f += dinicDFS(S, LLINF);
-	return f;
-}
+	ll maxflow(int s, int t, ll f = 0) {
+		while (1) {
+			fill(all(H), 0); H[s] = 1;
+			queue<int> q; q.push(s);
+			while (!q.empty()) {
+				int v = q.front(); q.pop();
+				for (int w : E[v]) if (G[w].f < G[w].c && !H[G[w].t])
+					H[G[w].t] = H[v] + 1, q.push(G[w].t);
+			}
+			if (!H[t]) return f;
+			fill(all(P), 0);
+			while (ll df = dfs(t, s, LLINF)) f += df;
+		}
+	}
+};
