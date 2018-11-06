b25e479184e788b457bdf43dea6c36af2d8561c1
Ludo Pulles <ludo.pulles@gmail.com>
2018-11-06 19:51:32 +0100

    Stress assertion in Binary Search

--- code/binary_search.cpp
+++ code/binary_search.cpp
@@ -1,7 +1,7 @@
 bool test(int n);
 
 int search(int lo, int hi) {
-	// assert(test(lo) && !test(hi));
+	assert(test(lo) && !test(hi)); // BE CERTAIN
 	while (hi - lo > 1) {
 		int m = (lo + hi) / 2;
 		(test(m) ? lo : hi) = m;
