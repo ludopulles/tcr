66937f45671fb1a0297a31896f988e2a4b93dc0e
MadPidgeon <daanvangent@gmail.com>
2018-11-18 11:34:32 +0100

    Update dijkstra.cpp
    Less C, More ++

--- code/graphs/dijkstra.cpp
+++ code/graphs/dijkstra.cpp
@@ -1,20 +1,16 @@
-int *dist, *dad;
-struct cmp {
-  bool operator()(int a, int b) {
-    return dist[a] != dist[b] ? dist[a] < dist[b] : a < b; }
-};
-pair<int*, int*> dijkstra(int n, int s, vii *adj) {
-  dist = new int[n];
-  dad = new int[n];
-  rep(i,0,n) dist[i] = INF, dad[i] = -1;
-  set<int, cmp> pq;
-  dist[s] = 0, pq.insert(s);
-  while (!pq.empty()) {
-    int cur = *pq.begin(); pq.erase(pq.begin());
-    rep(i,0,size(adj[cur])) {
-      int nxt = adj[cur][i].first,
-        ndist = dist[cur] + adj[cur][i].second;
-      if (ndist < dist[nxt]) pq.erase(nxt),
-        dist[nxt] = ndist, dad[nxt] = cur, pq.insert(nxt);
-    } }
-  return pair<int*, int*>(dist, dad); }
+#define INFTY -1
+vi dijkstra( vector<vii> G, ll s ) {
+	vi d( G.size(), INFTY );
+	priority_queue<ii,vector<ii>,greater<ii>> Q;
+	Q.emplace(0,s);
+	while(!Q.empty()){
+		ll c = Q.top().x, a = Q.top().y;
+		Q.pop();
+		if(d[a] != INFTY)
+			continue;
+		d[a] = c;
+		for(ii e : G[a])
+			Q.emplace(d[a] + e.y, e.x);
+	}
+	return d;
+}
