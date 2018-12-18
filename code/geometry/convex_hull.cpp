df1a783b012029622ab1e8d047ad936f09b3aac8
Ludo Pulles <ludo.pulles@gmail.com>
2018-12-18 19:31:45 +0100

    Fix the convex hull, remove some points from it.

--- code/geometry/convex_hull.cpp
+++ code/geometry/convex_hull.cpp
@@ -9,12 +9,13 @@ vi convexHull(const vector<pt> &pts) {
 		pt p = pts[a] - pts[st], q = pts[b] - pts[st];
 		return (p ^ q) != 0 ? (p ^ q) > 0 : lenSq(p) < lenSq(q);
 	});
-	ret.pb(st);
+	ord.pb(st); ret.pb(st);
 	for (int i : ord) {
 		// use '>' to include ALL points on the hull-line
 		for (int s = ret.size() - 1; s > 0 && ((pts[ret[s-1]] - pts[ret[s]]) ^ (pts[i] - pts[ret[s]])) >= 0; s--)
 			ret.pop_back();
 		ret.pb(i);
 	}
+	ret.pop_back();
 	return ret;
 }
