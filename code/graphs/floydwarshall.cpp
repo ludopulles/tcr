6db04b54833cb9934965048883638d3ddae439e2
MadPidgeon <daanvangent@gmail.com>
2019-03-14 14:46:02 +0100

    Update floydwarshall.cpp

--- code/graphs/floydwarshall.cpp
+++ code/graphs/floydwarshall.cpp
@@ -2,7 +2,7 @@
 void floyd_warshall( vvi& d ) {
 	ll n = d.size();
 	rep(i,0,n) rep(j,0,n) rep(k,0,n) 
-		if( d[j][i] < INFTY and d[i][k] < INFTY )
+		if( d[j][i] < INFTY and d[i][k] < INFTY ) // !!! neg. edges
 			d[j][k] = max(-INFTY,min(d[j][k],d[j][i]+d[i][k]));
 }
 vvi d(n,vi(n,INFTY)); rep(i,0,n) d[i][i] = 0;
