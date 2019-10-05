38dd6f050a753866c64f3ce8e446f9f36e7786da
Ludo Pulles <ludo.pulles@gmail.com>
2019-10-05 23:00:14 +0200

    Small fixes

--- code/graphs/stable.cpp
+++ code/graphs/stable.cpp
@@ -1,29 +1,24 @@
-// n = aantal mannen, v = aantal vrouwen
+// n = aantal mannen, m = aantal vrouwen
 // voor een man i, is order[i] de prefere
-vi stable(int n, int m, vvi order, vvi pref){
+vi stable(int n, int m, vvi order, vvi pref) {
 	queue<int> q;
 	rep(i,0,n)
 		q.push(i);
 	vi mas(m,-1), mak(n,-1), p(n,0);
-	while(!q.empty()){
+	while (!q.empty()) {
 		int k = q.front();
 		q.pop();
-		int s = order[k][p[k]];
-		if(mas[s] == -1){
+		int s = order[k][p[k]], k2 = mas[s];
+		if (mas[s] == -1) {
 			mas[s] = k;
 			mak[k] = s;
-		}
-		else{
-			int k2 = mas[s];
-			if(pref[k][s] < pref[k2][s]){
-				mas[s] = k;
-				mak[k] = s;
-				mak[k2] = -1;
-				q.push(k2);
-			}
-			else{
-				q.push(k);
-			}
+		} else if (pref[k][s] < pref[k2][s]) {
+			mas[s] = k;
+			mak[k] = s;
+			mak[k2] = -1;
+			q.push(k2);
+		} else {
+			q.push(k);
 		} 
 		p[k]++;	
 	}
