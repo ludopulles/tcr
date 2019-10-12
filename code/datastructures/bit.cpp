a0747cf0bf97e1cdef4522c0f23edc0b0e447f0d
Ludo Pulles <ludo.pulles@gmail.com>
2019-10-13 01:21:14 +0200

    Binary Indexed Tree ook met range query gemaakt

--- code/datastructures/bit.cpp
+++ code/datastructures/bit.cpp
@@ -1,14 +1,31 @@
 struct BIT {
-	int n;
-	vector<ll> A;
+	int n; vi A;
+	BIT(int _n) : n(_n), A(_n+1, 0) {}
+	BIT(vi &v) : n(sz(v)), A(1) {
+		for (auto x:v) A.pb(x);
+		for (int i=1, j; j=i&-i, i<=n; i++)
+			if (i+j <= n) A[i+j] += A[i];
+	}
+	void update(int i, ll v) { // a[i] += v
+		while (i <= n) A[i] += v, i += i&-i;
+	}
+	ll query(int i) { // sum_{j<=i} a[j]
+		ll v = 0;
+		while (i) v += A[i], i -= i&-i;
+		return v;
+	}
+};
 
-	BIT(int _n) : n(_n), A(n, 0) {}
-	// A[i] += v
-	void update(int i, ll v) {
-		while (i < n) A[i] += v, i += i & -i;
+struct rangeBIT {
+	int n; BIT b1, b2;
+	rangeBIT(int _n) : n(_n), b1(_n), b2(_n+1) {}
+	rangeBIT(vi &v) : n(sz(v)), b1(v), b2(sz(v)+1) {}
+	void pupdate(int i, ll v) { b1.update(i, v); }
+	void rupdate(int i, int j, ll v) { // a[i,..,j]+=v
+		b2.update(i, v);
+		b2.update(j+1, -v);
+		b1.update(j+1, v*j);
+		b1.update(i, (1-i)*v);
 	}
-	// returns sum_{0<j<=i} A[j]
-	ll query(int i) {
-		ll v = 0; while (i > 0) v += A[i], i -= i & -i; return v;
-	}
+	ll query(int i) {return b1.query(i)+b2.query(i)*i;}
 };
