0c63d4c2c5671293493b9a4b208559b9e3986411
Ludo Pulles <ludo.pulles@gmail.com>
2019-10-13 21:15:03 +0200

    Meetkunde deel aangepast om geen vec/frac te gebruiken

--- code/geometry/main.cpp
+++ code/geometry/main.cpp
@@ -1,95 +1,105 @@
-const double EPS = 1e-7, PI = acos(-1.0);
-
-typedef long long NUM; // EITHER double OR long long
+const ld EPS = 1e-7, PI = acos(-1.0);
+typedef ld NUM; // EITHER ld OR ll
 typedef pair<NUM, NUM> pt;
-#define x first
-#define y second
 
-pt operator+(pt p,pt q){return pt(p.x+q.x, p.y+q.y);}
-pt operator-(pt p,pt q){return pt(p.x-q.x, p.y-q.y);}
+pt operator+(pt p,pt q){ return {p.x+q.x,p.y+q.y}; }
+pt operator-(pt p,pt q){ return {p.x-q.x,p.y-q.y}; }
+pt operator*(pt p, NUM n) { return {p.x*n, p.y*n}; }
 
 pt& operator+=(pt &p, pt q) { return p = p+q; }
 pt& operator-=(pt &p, pt q) { return p = p-q; }
 
-pt operator*(pt p,NUM l) { return pt(p.x*l, p.y*l); }
-pt operator/(pt p,NUM l) { return pt(p.x/l, p.y/l); }
+NUM operator*(pt p, pt q){ return p.x*q.x+p.y*q.y; }
+NUM operator^(pt p, pt q){ return p.x*q.y-p.y*q.x; }
 
-NUM operator*(pt p, pt q) { return p.x*q.x+p.y*q.y; }
-NUM operator^(pt p, pt q) { return p.x*q.y-p.y*q.x; }
-
-NUM lenSq(pt p) { return p * p; }
-NUM lenSq(pt p, pt q) { return lenSq(p - q); }
-double len(pt p) { return hypot(p.x, p.y); }
-double len(pt p, pt q) { return len(p - q); }
-
-typedef pt frac;
-typedef pair<double, double> vec;
-vec getvec(pt p, pt dp, frac t) { return vec(p.x + 1. * dp.x * t.x / t.y, p.y + 1. * dp.y * t.x / t.y); }
-
-// square distance from pt a to line bc
-frac distPtLineSq(pt a, pt b, pt c) {
-	a -= b, c -= b;
-	return frac((a ^ c) * (a ^ c), c * c);
+// square distance from p to line ab
+ld distPtLineSq(pt p, pt a, pt b) {
+	p -= a; b -= a;
+	return ld(p^b) * (p^b) / (b*b);
+}
+// square distance from p to linesegment ab
+ld distPtSegmentSq(pt p, pt a, pt b) {
+	p -= a; b -= a;
+	NUM dot = p*b, len = b*b;
+	if (dot <= 0) return p*p;
+	if (dot >= len) return (p-b)*(p-b);
+	return p*p - ld(dot)*dot/len;
+}
+// Test if p is on line segment ab
+bool segmentHasPoint(pt p, pt a, pt b) {
+	pt u = p-a, v = p-b;
+	return abs(u^v) < EPS && u*v <= 0;
 }
 
-// square distance from pt a to linesegment bc
-frac distPtSegmentSq(pt a, pt b, pt c) {
-	a -= b; c -= b;
-	NUM dot = a * c, len = c * c;
-	if (dot <= 0) return frac(a * a, 1);
-	if (dot >= len) return frac((a - c) * (a - c), 1);
-	return frac(a * a * len - dot * dot, len);
+// projects p onto the line ab
+pair<ld,ld> proj(pt p, pt a, pt b) {
+	p -= a; b -= a;
+	return a + b*(ld(b*p) / (b*b));
 }
 
-// projects pt a onto linesegment bc
-frac proj(pt a, pt b, pt c) { return frac((a - b) * (c - b), (c - b) * (c - b)); }
-vec projv(pt a, pt b, pt c) { return getvec(b, c - b, proj(a, b, c)); }
+bool col(pt a, pt b, pt c) {
+	return abs((a-b) ^ (a-c)) < EPS;
+}
 
-bool collinear(pt a, pt b, pt c) { return ((a - b) ^ (a - c)) == 0; }
+// true => 1 intersection, false => parallel or same
+bool linesIntersect(pt a, pt b, pt c, pt d) {
+	return abs((a-b) ^ (c-d)) > EPS;
+}
 
-// true => 1 intersection, false => parallel, so 0 or \infty solutions
-bool linesIntersect(pt a, pt b, pt c, pt d) { return ((a - b) ^ (c - d)) != 0; }
-vec lineLineIntersection(pt a, pt b, pt c, pt d) {
-	double det = (a - b) ^ (c - d); pt ret = (c - d) * (a ^ b) - (a - b) * (c ^ d);
-	return vec(ret.x / det, ret.y / det);
+pair<ld,ld> lineLineIntersection(pt a, pt b, pt c, pt d) {
+	ld det = (a-b) ^ (c-d);
+	assert(abs(det) > EPS);
+	return ((c-d)*(a^b) - (a-b)*(c^d)) * (ld(1.0)/det);
 }
 
 // dp, dq are directions from p, q
 // intersection at p + t_i dp, for 0 <= i < return value
-int segmentIntersection(pt p, pt dp, pt q, pt dq, frac &t0, frac &t1){
-	if (dp * dp == 0) swap(p, q), swap(dp, dq); // dq = 0
-	if (dp * dp == 0) { t0 = t1 = frac(0, 1); return p == q; } // dp = dq = 0
-	pt dpq = (q - p); NUM c = dp ^ dq, c0 = dpq ^ dp, c1 = dpq ^ dq;
-	if (c == 0) { // parallel, dp > 0, dq >= 0
-		if (c0 != 0) return 0; // not collinear
-		NUM v0 = dpq * dp, v1 = v0 + dq * dp, dp2 = dp * dp;
+int segmentIntersection(pt p, pt dp, pt q, pt dq,
+		pt &A, pt &B) {
+	if (abs(dp * dp)<EPS)
+		swap(p,q), swap(dp,dq); // dq=0
+	if (abs(dp * dp)<EPS) {
+		A = p; // dp = dq = 0
+		return p == q;
+	}
+
+	pt dpq = q-p;
+	NUM c = dp^dq, c0 = dpq^dp, c1 = dpq^dq;
+	if (abs(c) < EPS) { // parallel, dp > 0, dq >= 0
+		if (abs(c0) > EPS) return 0; // not collinear
+		NUM v0 = dpq*dp, v1 = v0 + dq*dp, dp2 = dp*dp;
 		if (v1 < v0) swap(v0, v1);
-		t0 = frac(v0 = max(v0, (NUM) 0), dp2);
-		t1 = frac(v1 = min(v1, dp2), dp2);
+
+		v0 = max(v0, NUM(0));
+		v1 = min(v1, dp2);
+
+		A = p + dp * (ld(v0) / dp2);
+		B = p + dp * (ld(v1) / dp2);
+
 		return (v0 <= v1) + (v0 < v1);
-	} else if (c < 0) c = -c, c0 = -c0, c1 = -c1;
-	t0 = t1 = frac(c1, c);
-	return 0 <= min(c0, c1) && max(c0, c1) <= c;
+	}
+
+	if (c < 0) {
+		c = -c; c0 = -c0; c1 = -c1;
+	}
+
+	A = p + dp * (ld(c1)/c);
+	return 0 <= min(c0,c1) && max(c0,c1) <= c;
 }
 
-// Returns TWICE the area of a polygon to keep it an integer
-NUM polygonTwiceArea(const vector<pt> &pts) {
+// Returns TWICE the area of a polygon (for integers)
+NUM polygonTwiceArea(const vector<pt> &p) {
 	NUM area = 0;
-	for (int N = pts.size(), i = 0, j = N - 1; i < N; j = i++)
-		area += pts[i] ^ pts[j];
-	return abs(area); // area < 0 <=> pts ccw
-}
-
-bool segmenthaspt(pt s, pt e, pt p) {
-	pt ds = p-s, de = p-e;
-	return (ds ^ de) == 0LL && (ds * de) <= 0LL;
+	for (int n = sz(p), i=0, j=n-1; i<n; j = i++)
+		area += p[i] ^ p[j];
+	return abs(area); // area < 0 <=> p ccw
 }
 
 bool insidePolygon(const vector<pt> &pts, pt p, bool strict = true) {
 	int n = 0;
-	for (int N = pts.size(), i = 0, j = N - 1; i < N; j = i++) {
+	for (int N = sz(pts), i = 0, j = N - 1; i < N; j = i++) {
 		// if p is on edge of polygon
-		if (segmenthaspt(pts[i], pts[j], p)) return !strict;
+		if (segmentHasPoint(p, pts[i], pts[j])) return !strict;
 		// or: if(distPtSegmentSq(p, pts[i], pts[j]) <= EPS) return !strict;
 
 		// increment n if segment intersects line from p
