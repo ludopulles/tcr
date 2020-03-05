956cc3ce624c780df831f2fa2d1a445ac8d3d5c8
Ludo Pulles <ludo.pulles@gmail.com>
2020-03-05 22:00:37 +0100

    Use linking by size instead of rank -> -3 lines

--- code/datastructures/dsu.cpp
+++ code/datastructures/dsu.cpp
@@ -1,12 +1,9 @@
-struct dsu {
-	vi par, rnk;
-	dsu(int n) : par(n, -1), rnk(n, 0) {}
-	int find(int i) { return
-		par[i] < 0 ? i : par[i] = find(par[i]); }
+struct dsu { vi p; dsu(int n) : p(n, -1) {}
+	int find(int i) {
+		return p[i] < 0 ? i : p[i] = find(p[i]); }
 	void unite(int a, int b) {
 		if ((a = find(a)) == (b = find(b))) return;
-		if (rnk[a] < rnk[b]) swap(a, b);
-		if (rnk[a] == rnk[b]) rnk[a]++;
-		par[a] += par[b]; par[b] = a;
+		if (p[a] > p[b]) swap(a, b);
+		p[a] += p[b]; p[b] = a;
 	}
 };
